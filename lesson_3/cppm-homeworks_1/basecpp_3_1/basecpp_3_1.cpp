5771f9eb40a1c4cb4cfa49bf0e3a8de064e9757f
Nikitosovich <nikitaand87@mail.ru>
2022-08-15 21:58:23 +0300

    Классы и объекты

---
+++ lesson_3/cppm-homeworks_1/basecpp_3_1/basecpp_3_1.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+using namespace std;
+
+class Calcucator
+{
+private:
+	double num1, num2;
+public:
+	bool set_num1(double num1) {
+		bool result = false;
+		if (num1 != 0) {
+			this->num1 = num1;
+			result = true;
+		}
+		return result;
+	}
+	bool set_num2(double num2) {
+		bool result = false;
+		if (num2 != 0) {
+			this->num2 = num2;
+			result = true;
+		}
+		return result;
+	}
+	double add() {
+		double result = num1 + num2;
+		return result;
+	}
+	double subtract_1_2() {
+		return num1 - num2;
+	}
+	double subtract_2_1() {
+		return num2 - num1;
+	}
+	double multiply() {
+		return num1 * num2;
+	}
+	double divide_1_2() {
+		return num1 / num2;
+	}
+	double divide_2_1() {
+		return num2 / num1;
+	}
+
+
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	Calcucator calc;
+	
+	while (true)
+	{
+		double x = 0;
+		cout << "Введите num1: ";
+		cin >> x;
+		if (calc.set_num1(x))
+			break;
+		else cout << "не верное число\n";
+
+	}
+
+	while (true)
+	{
+		double y = 0;
+		cout << "Введите num2: ";
+		cin >> y;
+		if (calc.set_num2(y))
+			break;
+		else cout << "не верное число\n";
+
+	}
+	cout << "num1 + num2 = " << calc.add() << endl;
+	cout << "num1 - num2 = " << calc.subtract_1_2() << endl;
+	cout << "num2 - num1 = " << calc.subtract_2_1() << endl;
+	cout << "num1 * num2 = " << calc.multiply() << endl;
+	cout << "num1 / num2 = " << calc.divide_1_2() << endl;
+	cout << "num2 / num1 = " << calc.divide_2_1() << endl;
+	system("pause");
+	return 0;
+}
+
---
+++ lesson_3/cppm-homeworks_2/basecpp_3_2/basecpp_3_2.cpp
@@ -0,0 +1,78 @@
+﻿#include <iostream>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+class Counter
+{
+private:
+	int count;
+public:
+	Counter() :count(1)
+	{}
+	Counter(int count) {
+		this->count = count;
+	}
+	void inc_count()
+	{
+		count++;
+	}
+	void dec_count()
+	{
+		count--;
+	}
+	int get_count()
+	{
+		return count;
+	}
+};
+
+void func(Counter coun) {
+	char a = 0;
+	while (true)
+	{
+		cout << "Введите команду ('+', '-', '=' или 'x'): ";
+		cin >> a;
+		if (a == 'х') {
+			cout << "До свидания!" << endl;
+			break;
+		}
+		switch (a)
+		{
+		case '+': coun.inc_count();
+			break;
+		case '-': coun.dec_count();
+			break;
+		case '=': cout << coun.get_count() << endl;
+			break;
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: " << endl;
+	string input;
+	cin >> input;
+
+	if (input == "да")
+	{
+		int x = 0;
+		cout << "Введите начальное значение счётчика: ";
+		cin >> x;
+		Counter coun2(x);
+		func(coun2);
+	}
+	else if (input == "нет") {
+		Counter coun1;
+		func(coun1);
+	}
+	cout << "Работа завершена!";
+
+	system("pause");
+	return 0;
+}
