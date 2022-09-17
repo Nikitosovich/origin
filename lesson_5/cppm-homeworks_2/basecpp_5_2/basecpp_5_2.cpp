72b74b06554ceee7d6f33bda372ec85a3d74b60e
Nikitosovich <nikitaand87@mail.ru>
2022-09-17 21:43:33 +0300

    После доработки 2

--- lesson_5/cppm-homeworks_2/basecpp_5_2/basecpp_5_2.cpp
+++ lesson_5/cppm-homeworks_2/basecpp_5_2/basecpp_5_2.cpp
@@ -14,9 +14,9 @@ int main()
 	setlocale(LC_ALL, "ru");
 
 	Triangle triangle(10, 20, 30, 50, 60, 70);
-	Right_triangle right_triangle(10, 20, 30, 50, 60, 90);
-	Isosceles_triangle isosceles_triangle(10, 20, 10, 50, 60, 50);
-	Equilateral_triangle equilateral_triangle(30, 30, 30, 60, 60, 60);
+	Right_triangle right_triangle(10, 20, 30, 50, 60);
+	Isosceles_triangle isosceles_triangle(10, 20, 50, 60);
+	Equilateral_triangle equilateral_triangle(30, 60);
 
 	print(&triangle);
 	print(&right_triangle);
@@ -24,10 +24,10 @@ int main()
 	print(&equilateral_triangle);
 
 	Quadrangle quadrangle(10, 20, 30, 40, 50, 60, 70, 80);
-	Rectangle rectangle(10, 20, 10, 20, 90, 90, 90, 90);
-	Square square(20, 20, 20, 20, 90, 90, 90, 90);
-	Parallelogram parallelogram(20, 30, 20, 30, 30, 40, 30, 40);
-	Rhomb rhomb(30, 30, 30, 30, 30, 40, 30, 40);
+	Rectangle rectangle(10, 20);
+	Square square(20);
+	Parallelogram parallelogram(20, 30, 30, 40);
+	Rhomb rhomb(30, 30, 40);
 
 	print(&quadrangle);
 	print(&rectangle);
--- lesson_5/cppm-homeworks_2/basecpp_5_2/derived_quadrangle.h
+++ lesson_5/cppm-homeworks_2/basecpp_5_2/derived_quadrangle.h
@@ -4,95 +4,31 @@
 class Rectangle :public Quadrangle {
 public:
 
-	Rectangle(uint32 a, uint32 b, uint32 c, uint32 d, uint32 A, uint32 B, uint32 C, uint32 D)
-		:Quadrangle(a, b, c, d, A, B, C, D, "Прямоугольник") {
-	}
-
-	virtual std::string print_string() {
-		std::string result = "";
-		if ((this->A != 90) ||
-			(this->B != 90) ||
-			(this->C != 90) ||
-			(this->D != 90) ||
-			(this->a != this->c) ||
-			(this->b != this->d)) {
-			this->~Rectangle();
-		}
-		else {
-			result = f_format();
-		}
-		return result;
+	Rectangle(uint32 a, uint32 b)
+		:Quadrangle(a, b, a, b, 90, 90, 90, 90, "Прямоугольник") {
 	}
 };
 
 class Square :public Quadrangle {
 public:
 
-	Square(uint32 a, uint32 b, uint32 c, uint32 d, uint32 A, uint32 B, uint32 C, uint32 D)
-		:Quadrangle(a, b, c, d, A, B, C, D, "Квадрат") {
-	}
-
-	virtual std::string print_string() {
-		std::string result = "";
-		if ((this->A != 90) ||
-			(this->B != 90) ||
-			(this->C != 90) ||
-			(this->D != 90) ||
-			(this->a != this->b) ||
-			(this->b != this->c) ||
-			(this->c != this->d) ||
-			(this->d != this->a)) {
-			this->~Square();
-		}
-		else {
-			result = f_format();
-		}
-		return result;
+	Square(uint32 a)
+		:Quadrangle(a, a, a, a, 90, 90, 90, 90, "Квадрат") {
 	}
 };
 
 class Parallelogram :public Quadrangle {
 public:
 
-	Parallelogram(uint32 a, uint32 b, uint32 c, uint32 d, uint32 A, uint32 B, uint32 C, uint32 D)
-		:Quadrangle(a, b, c, d, A, B, C, D, "Параллелограмм") {
-	}
-
-	virtual std::string print_string() {
-		std::string result = "";
-		if ((this->A != this->C) ||
-			(this->B != this->D) ||
-			(this->a != this->c) ||
-			(this->b != this->d)) {
-			this->~Parallelogram();
-		}
-		else {
-			result = f_format();
-		}
-		return result;
+	Parallelogram(uint32 a, uint32 b, uint32 A, uint32 B)
+		:Quadrangle(a, b, a, b, A, B, A, B, "Параллелограмм") {
 	}
 };
 
 class Rhomb :public Quadrangle {
 public:
 
-	Rhomb(uint32 a, uint32 b, uint32 c, uint32 d, uint32 A, uint32 B, uint32 C, uint32 D)
-		:Quadrangle(a, b, c, d, A, B, C, D, "Ромб") {
-	}
-
-	virtual std::string print_string() {
-		std::string result = "";
-		if ((this->A != this->C) ||
-			(this->B != this->D) ||
-			(this->a != this->b) ||
-			(this->b != this->c) ||
-			(this->c != this->d) ||
-			(this->d != this->a)) {
-			this->~Rhomb();
-		}
-		else {
-			result = f_format();
-		}
-		return result;
+	Rhomb(uint32 a, uint32 A, uint32 B)
+		:Quadrangle(a, a, a, a, A, B, A, B, "Ромб") {
 	}
 };
