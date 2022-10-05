c091c258221501a943cb25c2728d65398d402e82
Nikitosovich <nikitaand87@mail.ru>
2022-10-05 22:47:51 +0300

    Новшества С++

---
+++ cppl_lesson_1/cppl-homeworks_1/cppl_1_1/cppl_1_1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using std::for_each;
+int main() {
+
+	std::vector<int> vec{
+		4, 7, 9, 14, 12
+	};
+
+	int count = 0;
+	for_each(vec.begin(), vec.end(), [&vec, count](int num)mutable {
+		if (num % 2 != 0)
+		{
+			num *= 3;
+			vec[count] = num;
+		}
+		std::cout << vec[count] << " ";
+		++count;
+		});
+
+	return 0;
+}
---
+++ cppl_lesson_1/cppl-homeworks_2/cppl_1_2/cppl_1_2.cpp
@@ -0,0 +1,51 @@
+﻿
+#include <iostream>
+#include <variant>
+#include <string>
+#include <vector>
+
+std::variant<int, std::string, std::vector<int>> get_variant() {
+	std::srand(std::time(nullptr));
+	int random_variable = std::rand() % 3;
+
+	std::variant<int, std::string, std::vector<int>> result;
+	switch (random_variable)
+	{
+	case 0:
+		result = 5;
+		break;
+	case 1:
+		result = "string";
+		break;
+	case 2:
+		result = std::vector<int>{ 1, 2, 3, 4, 5 };
+		break;
+	default:
+		break;
+	}
+	return result;
+}
+
+int main()
+{
+	std::variant<int, std::string, std::vector<int>> result;
+	std::string* pVal;
+	result = get_variant();
+	if (std::holds_alternative<int>(result))
+		std::cout << std::get<int>(result) * 2;
+	else if ((pVal = std::get_if<std::string>(&result)) != nullptr)
+		std::cout << *pVal;
+	else {
+		try
+		{
+			std::vector<int> vec = std::get<std::vector<int>>(result);
+			for (int num : vec) {
+				std::cout << num << ", ";
+			}
+		}
+		catch (const std::bad_variant_access& ex) { /*error*/ }
+	}
+
+	return 0;
+}
+
