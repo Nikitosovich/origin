52f47cc24e26d5ebc567b6bbbb40a9291e85a96f
Nikitosovich <nikitaand87@mail.ru>
2022-10-21 16:31:04 +0300

    Тестирование

---
+++ cppl_lesson_4/cppl-homeworks_2/cppl_4_2/cppl_4_2.cpp
@@ -0,0 +1,158 @@
+﻿#pragma once
+#include <iostream>
+#include <cstring>
+#include <limits.h>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/catch_session.hpp>
+
+struct ListNode
+{
+public:
+	ListNode(int value, ListNode* prev = nullptr, ListNode* next = nullptr)
+		: value(value), prev(prev), next(next)
+	{
+		if (prev != nullptr) prev->next = this;
+		if (next != nullptr) next->prev = this;
+	}
+
+public:
+	int value;
+	ListNode* prev;
+	ListNode* next;
+};
+
+class List
+{
+public:
+	List()
+		: m_head(new ListNode(static_cast<int>(0))), m_size(0),
+		m_tail(new ListNode(0, m_head))
+	{
+	}
+
+	virtual ~List()
+	{
+		Clear();
+		delete m_head;
+		delete m_tail;
+	}
+
+	bool Empty() { return m_size == 0; }
+
+	unsigned long Size() { return m_size; }
+
+	void PushFront(int value)
+	{
+		new ListNode(value, m_head, m_head->next);
+		++m_size;
+	}
+
+	void PushBack(int value)
+	{
+		new ListNode(value, m_tail->prev, m_tail);
+		++m_size;
+	}
+
+	int PopFront()
+	{
+		if (Empty()) throw std::runtime_error("list is empty");
+		auto node = extractPrev(m_head->next->next);
+		int ret = node->value;
+		delete node;
+		return ret;
+	}
+
+	int PopBack()
+	{
+		if (Empty()) throw std::runtime_error("list is empty");
+		auto node = extractPrev(m_tail);
+		int ret = node->value;
+		delete node;
+		return ret;
+	}
+
+	void Clear()
+	{
+		auto current = m_head->next;
+		while (current != m_tail)
+		{
+			current = current->next;
+			delete extractPrev(current);
+		}
+	}
+
+private:
+	ListNode* extractPrev(ListNode* node)
+	{
+		auto target = node->prev;
+		target->prev->next = target->next;
+		target->next->prev = target->prev;
+		--m_size;
+		return target;
+	}
+
+private:
+	ListNode* m_head;
+	ListNode* m_tail;
+	unsigned long m_size;
+};
+
+TEST_CASE("Test", "[test List]")
+{
+	List list;
+
+	SECTION("PushBack") {
+		list.PushBack(5);
+		CHECK(list.PopBack() == 5);
+		list.PushBack(INT_MIN);
+		CHECK(list.PopBack() == INT_MIN);
+		list.PushBack(INT_MAX);
+		CHECK(list.PopBack() == INT_MAX);
+	}
+	SECTION("PushFront") {
+		list.PushFront(5);
+		CHECK(list.PopFront() == 5);
+		list.PushFront(INT_MIN);
+		CHECK(list.PopFront() == INT_MIN);
+		list.PushFront(INT_MAX);
+		CHECK(list.PopFront() == INT_MAX);
+
+	}
+	SECTION("PopBack") {
+		try {
+			int ret = list.PopBack();
+		}
+		catch (std::runtime_error& ex) {
+			std::string str = ex.what();
+			CHECK(str.compare("list is empty") == 0);
+		}
+		list.PushBack(5);
+		CHECK(list.PopBack() == 5);
+		list.PushBack(INT_MIN);
+		CHECK(list.PopBack() == INT_MIN);
+		list.PushBack(INT_MAX);
+		CHECK(list.PopBack() == INT_MAX);
+	}
+	SECTION("PopFront") {
+		try {
+			int ret = list.PopFront();
+		}
+		catch (std::runtime_error& ex) {
+			std::string str = ex.what();
+			CHECK(str.compare("list is empty") == 0);
+		}
+		list.PushFront(5);
+		CHECK(list.PopFront() == 5);
+		list.PushFront(INT_MIN);
+		CHECK(list.PopFront() == INT_MIN);
+		list.PushFront(INT_MAX);
+		CHECK(list.PopFront() == INT_MAX);
+	}
+}
+
+int main(int argc, char* argv[]) {
+
+	Catch::Session().run(argc, argv);
+
+	return 0;
+}
