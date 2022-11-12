984218b2611ae7eb5e4ce49469e2577166b55ee7
Nikitosovich <nikitaand87@mail.ru>
2022-11-12 21:42:11 +0300

    STL. Часть 1

---
+++ cppl_lesson_7/cppl-homeworks_1/cppl_7_1/cppl_7_1.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+#include <map>
+#include <vector>
+#include <algorithm>
+#include <format>
+
+/*Сортируем по убыванию*/
+bool pred(const std::pair<char, int>& a, const std::pair<char, int>& b) {
+	return a.second > b.second;
+}
+
+void print(std::vector< std::pair< int, int >>& vec) {
+	std::cout << "[IN]: Hello world!!" << std::endl << "[OUT]:" << std::endl;
+	for (auto i : vec) {
+		std::cout << std::format("{}: {}\n", static_cast<char>(i.first), i.second);
+	}
+}
+
+/*1. Получаем уникальные значения из строки в контейнер set.
+  2. Создадим map с ключем уникальных символов и количеством его повторений в строке */
+std::map<char, int> count_characters(const std::string& str) {
+	std::set<char> myset;
+
+	for (auto char_string : str) {
+		myset.insert(char_string);
+	}
+
+	std::map<char, int> mymap;
+	int count = 0;
+	for (auto char_set : myset) {
+		for (auto char_string : str) {
+			if (char_string == char_set) {
+				++count;
+			}
+		}
+		mymap[char_set] = count;
+		count = 0;
+	}
+	return mymap;
+}
+
+int main()
+{
+	std::string str = "Hello world";
+	/*std::string str = "aaaaajjjm";*/
+	std::map<char, int> mymap = count_characters(str);
+
+	std::vector< std::pair< int, int > > vec(mymap.begin(), mymap.end());
+	std::sort(vec.begin(), vec.end(), pred);
+
+	print(vec);
+
+	return 0;
+}
+
---
+++ cppl_lesson_7/cppl-homeworks_2/cppl_7_2/cppl_7_2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <set>
+
+int main()
+{
+	int num = 0;
+	int count = 0;
+	std::set<int,std::greater<int>> my_set;
+	std::cout << "[IN]:" << std::endl;
+	std::cin >> count;
+	for (int i = 0; i < count; ++i) {
+		std::cin >> num;
+		my_set.insert(num);
+	}
+
+	std::cout << "[OUT]:" << std::endl;
+	for (const auto& elem : my_set) {
+		std::cout << elem << std::endl;
+	}
+
+	return 0;
+}
